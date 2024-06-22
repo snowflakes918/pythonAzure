6dc738aea0ae5a8cb5cf22cc6d5ce29035a0468e
huangdavid <2461619116@qq.com>
2024-06-22 12:42:38 +0100

    user manage app

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from models import db, User
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
 
 @app.route('/')
-def hello_world():
-    return 'bulibulibuli'
+def index():
+    users = User.query.all()
+    return render_template('index.html', users=users)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        new_user = User(name=name, email=email)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add_user.html')
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    print('Hello')
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
